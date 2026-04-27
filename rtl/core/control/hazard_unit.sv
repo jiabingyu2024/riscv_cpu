dc268046deb5ad1b0befc66383515df67e4fe63d
jiabingyu2024 <jiabing.yu2024@gmail.com>
2026-04-27 11:10:41 +0800

    fix the load-use path

--- rtl/core/control/hazard_unit.sv
+++ rtl/core/control/hazard_unit.sv
@@ -14,14 +14,13 @@ module hazard_unit(
     input  logic                            i_clk,
     input  logic                            i_rst_n,
     input  logic  [`PC_BUS]                 i_pc_cur,
+    input  logic  [`RF_BUS]                 i_rs1_addr_f,
+    input  logic  [`RF_BUS]                 i_rs2_addr_f,
     input  logic  [`RF_BUS]                 i_rs1_addr_d,
     input  logic  [`RF_BUS]                 i_rs2_addr_d,
     input  logic  [`RF_BUS]                 i_rd_addr_e,
     input  logic                            i_mem_read_e,
     input  logic                            i_reg_write_e,//load_use
-    input  logic  [`RF_BUS]                 i_rd_addr_m,
-    input  logic                            i_mem_read_m,
-    input  logic                            i_reg_write_m,
 
     input  logic                            i_predict_taken,
     input  logic  [`PC_BUS]                 i_predict_target,
@@ -48,25 +47,27 @@ module hazard_unit(
 );
 
     logic load_use_hazard_raw_e;
-    logic load_use_hazard_raw_m;
-    logic load_use_hazard_hold;
+    logic load_use_hazard_raw_f;
+    logic load_use_hazard_hold_d;
+    logic load_use_hazard_hold_f;
     logic load_use_hazard;
 
     assign load_use_hazard_raw_e = i_mem_read_e && i_reg_write_e && (i_rd_addr_e != '0) &&
                                    ((i_rd_addr_e == i_rs1_addr_d) || (i_rd_addr_e == i_rs2_addr_d));
-    assign load_use_hazard_raw_m = i_mem_read_m && i_reg_write_m && (i_rd_addr_m != '0) &&
-                                   ((i_rd_addr_m == i_rs1_addr_d) || (i_rd_addr_m == i_rs2_addr_d));
-    assign load_use_hazard = load_use_hazard_raw_e || load_use_hazard_raw_m || load_use_hazard_hold;
+    assign load_use_hazard_raw_f = i_mem_read_e && i_reg_write_e && (i_rd_addr_e != '0) &&
+                                   ((i_rd_addr_e == i_rs1_addr_f) || (i_rd_addr_e == i_rs2_addr_f));
+    assign load_use_hazard = load_use_hazard_raw_e || load_use_hazard_hold_d || load_use_hazard_hold_f;
 
     always_ff @(posedge i_clk or negedge i_rst_n) begin
         if (!i_rst_n) begin
-            load_use_hazard_hold <= 1'b0;
+            load_use_hazard_hold_d <= 1'b0;
+            load_use_hazard_hold_f <= 1'b0;
         end else if (i_error) begin
-            load_use_hazard_hold <= 1'b0;
-        end else if (load_use_hazard_raw_e) begin
-            load_use_hazard_hold <= 1'b1;
+            load_use_hazard_hold_d <= 1'b0;
+            load_use_hazard_hold_f <= 1'b0;
         end else begin
-            load_use_hazard_hold <= 1'b0;
+            load_use_hazard_hold_d <= load_use_hazard_raw_e;
+            load_use_hazard_hold_f <= load_use_hazard_raw_f && !load_use_hazard_raw_e;
         end
     end
 
